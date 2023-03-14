ede95178d5c2d52eb8c1178e25a3196a75423e97
daviimt <dmateomerino@gmail.com>
2023-03-14 13:47:55 +0100

    finish contract

--- contracts/TasksContract.sol
+++ contracts/TasksContract.sol
@@ -1,9 +1,8 @@
-// SPDX-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
 contract TasksContract {
-
-    uint taskCounter=0;
+    uint256 public tasksCounter = 0;
 
     struct Task {
         uint256 id;
@@ -12,14 +11,46 @@ contract TasksContract {
         bool done;
         uint256 createdAt;
     }
+
+    event TaskCreated(
+        uint256 id,
+        string title,
+        string description,
+        bool done,
+        uint256 createdAt
+    );
+    event TaskToggledDone(uint256 id, bool done);
+
     mapping(uint256 => Task) public tasks;
+
+    constructor() {
+        createTask("my first task", "my first description");
+    }
 
     function createTask(string memory _title, string memory _description)
         public
     {
-        tasks[taskCounter]=Task(taskCounter,_title, _description, false, block.timestamp);
-        taskCounter++;
+        tasksCounter++;
+        tasks[tasksCounter] = Task(
+            tasksCounter,
+            _title,
+            _description,
+            false,
+            block.timestamp
+        );
+        emit TaskCreated(
+            tasksCounter,
+            _title,
+            _description,
+            false,
+            block.timestamp
+        );
     }
 
-    // function toggleDone(){}
+    function toggleDone(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.done = !_task.done;
+        tasks[_id] = _task;
+        emit TaskToggledDone(_id, _task.done);
+    }
 }
